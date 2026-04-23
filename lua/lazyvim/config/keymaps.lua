1d9ed82fefdd6208553ed49df2a3153a1b4041de
nowaylifer <nowaylifer@gmail.com>
2026-04-23 21:11:48 +0300

    refactor(keymaps): remove hardcoded diagnostic jump float option

--- lua/lazyvim/config/keymaps.lua
+++ lua/lazyvim/config/keymaps.lua
@@ -124,7 +124,6 @@ local diagnostic_goto = function(next, severity)
     vim.diagnostic.jump({
       count = (next and 1 or -1) * vim.v.count1,
       severity = severity and vim.diagnostic.severity[severity] or nil,
-      float = true,
     })
   end
 end
